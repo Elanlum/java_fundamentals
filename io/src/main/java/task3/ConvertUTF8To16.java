85a4736a60d5c523588e16e359dd3aa36ad2852c
Elanlum <piratone94@mail.ru>
2018-07-17 16:07:43 +0300

    Task 3 - Working with byte and character input-output streams

---
+++ io/src/main/java/task3/ConvertUTF8To16.java
@@ -0,0 +1,45 @@
+package task3;
+
+import java.io.*;
+
+public class ConvertUTF8To16 {
+
+    public static String text;
+
+    public static void read(String filename) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename), "UTF8"))) {
+            StringBuffer stringBuffer = new StringBuffer();
+//            while ((text = bufferedReader.readLine()) != null) {
+//                stringBuffer.append(text).append("\n");
+//                text = stringBuffer.toString();
+//                System.out.println(text);
+//            }
+            bufferedReader.lines().forEach(s -> stringBuffer.append(s).append("\n"));
+            text = stringBuffer.toString();
+            System.out.println(text);
+        } catch (UnsupportedEncodingException ee) {
+            ee.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeTo(String filename){
+        try(Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "UTF16"))){
+            writer.write(text);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ConvertUTF8To16.read("filesTask3/textUTF8.txt");
+        ConvertUTF8To16.writeTo("filesTask3/textUTF16.txt");
+    }
+}
---
+++ oop/src/test/java/ru/elanlum/task2/EmployeeWorkplaceTest.java
@@ -0,0 +1,12 @@
+package ru.elanlum.task2;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeWorkplaceTest {
+
+    @Test
+    public void getStationeryTotalCostTest() {
+//        assertThat(employeeWorkplace.getStationeryTotalCost(), is(219));
+
+    }
+}
